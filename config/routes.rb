9b7bd1d43a2f88ec642a7e9ed41e59ba76912f52
verrom <verevkin.roma@gmail.com>
2015-09-25 10:09:40 +0000

    layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+
+  match '/administration', to: 'static_pages#administration',    via: 'get'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
