a3dd0f66b43b8aee7a6b8704e7c0478133314c83
verrom <verevkin.roma@gmail.com>
2015-10-17 19:34:35 -0700

    Editing settings-2

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,8 @@ set :repo_url, "https://github.com/verrom/yurta24.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, deploy_to
+#set :deploy_to, deploy_to
+set :deploy_via, :copy
 
 # Default value for :scm is :git
 
