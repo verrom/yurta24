11f5387c9d4a299f5c39e6f94a1521c2e9c2a4d9
verrom <verevkin.roma@gmail.com>
2016-01-25 13:25:49 +0700

    Edit mailer settings

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,6 +2,6 @@ class AdminMailer < ApplicationMailer
 	default from: "info@yurta24.ru"
 
     def notification(contact_form)
-        mail(to: "yurta24@yandex.ru, kamaz-enisey@mail.ru", subject: 'Новое сообщение на сайте yurta24.ru')
+        mail(to: "verevkin.roma@gmail.com, verevkin_ra@yandex.ru, verrom@inbox.ru", subject: 'Новое сообщение на сайте yurta24.ru')
     end
 end
