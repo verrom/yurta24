54594a5608d335f580fc988735ec0fd904b6c4b4
Shutov Dmitry <dmitryashutov@gmail.com>
2016-12-28 23:23:18 +0700

    Add validates for presence of name and phone fields

--- app/controllers/popup_contact_forms_controller.rb
+++ app/controllers/popup_contact_forms_controller.rb
@@ -8,7 +8,7 @@ class PopupContactFormsController < ApplicationController
 	@popup_contact_form = PopupContactForm.new popup_contact_form_params
 	@popup_contact_form.save
 	redirect_to root_path
-	AdminMailer.notification(@popup_contact_form).deliver_now
+	#AdminMailer.notification(@popup_contact_form).deliver_now
  end
 
  def destroy
